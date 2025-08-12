14f8685a1ec02b0b455313d2dd27f4d767865a1e
Denys Samoilov <133922338+denys-samoilov@users.noreply.github.com>
2025-08-12 21:15:16 +0300

    Separated rolls for melee and ranged attacks

--- module/sheets/ik2d6CharacterSheet.mjs
+++ module/sheets/ik2d6CharacterSheet.mjs
@@ -30,97 +30,102 @@ export default class IK2d6ActorSheet extends ActorSheet {
     });
 
     // Handle attack rolls
-    html.find('.roll-attack').click(async ev => {
+    html.find('.roll-ranged-attack, .roll-melee-one-hand-attack, .roll-melee-two-hand-attack').click(ev => {
+      const button = $(ev.currentTarget);
+      const attModifier = button.data('att-modifier') || 0;
+      const pow = button.data('pow') || 0;
+      this._onRoll(ev, attModifier, pow);
+    });
+  }
 
-      if(!game.user.targets.size) {
-        ui.notifications.warn("Shooting, huh? You need to select a target first!");
-        return;
-      }
-      
+  // Attack rolls function
+  async _onRoll(event, attModifier, pow) {
+    event.preventDefault();
 
-      const li = $(ev.currentTarget).closest('.actor-item');
-      const itemId = li.data('item-id');
-      const item = this.actor.items.get(itemId);
+    // Target check
+    if (!game.user.targets.size) {
+      ui.notifications.warn("Attacking, huh? You need to select a target first!");
+      return;
+    }
 
-      // Initializing target actor
-      const targets = Array.from(game.user.targets);
-      const targetToken = targets[0];
-      const targetActor = targetToken.actor;
-
-      // Getting modifiers and pow
-      const attModifier = item.system.attModifier || 0;
-      const pow = item.system.pow || 0;
-
-      // Getting boosted attack and damage checkboxes
-      const boostedAtt = li.find('.boost-att-checkbox').is(':checked');
-      const boostedDmg = li.find('.boost-dmg-checkbox').is(':checked');
-
-      // Attack roll
-      let attDice = boostedAtt ? 3 : 2;
-      let attackRoll = new Roll(`${attDice}d6 + ${attModifier}`);
-      await attackRoll.evaluate({async: true});
+    // Item check
+    const li = $(event.currentTarget).closest('.actor-item');
+    const itemId = li.data('item-id');
+    const item = this.actor.items.get(itemId);
 
 
-      // Sending attack roll message
-      attackRoll.toMessage({
-        speaker: ChatMessage.getSpeaker({actor: this.actor}),
-        flavor: `${item.name} Attack Roll (${attDice}d6${boostedAtt ? ' Boosted' : ''})`
-      });
-
-      // Delay to simulate roll animation
-      await new Promise(resolve => setTimeout(resolve, 3000));
+    // Check if item is a melee weapon
+    if(item.type == "melee-weapon") {
+      pow += this.actor.system.str;
+    }
 
 
-      if (attackRoll.total < 7) {
-        ui.notifications.info(`${this.actor.name} shoots ${targetActor.name}... but misses!`);
-        return; // Exit early if no damage roll is needed
-      }
-      else {
-        ui.notifications.info(`${this.actor.name} shoots ${targetActor.name} and hits!`);   
-      }
+    // Target initialization
+    const targets = Array.from(game.user.targets);
+    const targetToken = targets[0];
+    const targetActor = targetToken.actor;
 
-      // Damage roll
-      let dmgDice = boostedDmg ? 3 : 2;
-      let damageRoll = new Roll(`${dmgDice}d6 + ${pow}`);
-      await damageRoll.evaluate({async: true});
+    // Boost elements initialization
+    const boostedAtt = li.find('.boost-att-checkbox').is(':checked');
+    const boostedDmg = li.find('.boost-dmg-checkbox').is(':checked');
 
-        
+    // Attack roll initialization
+    let attDice = boostedAtt ? 3 : 2;
+    let attackRoll = new Roll(`${attDice}d6 + ${attModifier}`);
+    await attackRoll.evaluate({ async: true });
 
-      damageRoll.toMessage({
-        speaker: ChatMessage.getSpeaker({actor: this.actor}),
-        flavor: `${item.name} Damage Roll (${dmgDice}d6${boostedDmg ? ' Boosted' : ''})`,
-      });
+    // Attack roll message
+    attackRoll.toMessage({
+      speaker: ChatMessage.getSpeaker({ actor: this.actor }),
+      flavor: `${item.name} ${item.type.capitalize()} Attack Roll (${attDice}d6${boostedAtt ? ' Boosted' : ''})`
+    });
 
-      // Check if there are at least two same dice in the damage roll
-      const diceResults = damageRoll.dice[0].results;
-      const hasPair = diceResults.some((result, index) => 
-        diceResults.slice(index + 1).some(otherResult => 
-          result.result === otherResult.result
-        )
-      );
-      
-      // If there is a pair, notify the user
-      if (hasPair) {
-        ui.notifications.info(`${this.actor.name} deals critical damage to ${targetActor.name}!`);
-      }
+    // Wait for 3 seconds
+    await new Promise(resolve => setTimeout(resolve, 3000));
 
-      // Delay to simulate roll animation
-      await new Promise(resolve => setTimeout(resolve, 3000));
+    // Attack result check
+    if (attackRoll.total < 7) {
+      ui.notifications.info(`${this.actor.name} attacks ${targetActor.name}... but misses!`);
+      return;
+    } else {
+      ui.notifications.info(`${this.actor.name} attacks ${targetActor.name} and hits!`);
+    }
 
+    // Critical hit check
+    const diceResults = attackRoll.dice[0].results;
+    const hasPair = diceResults.some((result, index) =>
+      diceResults.slice(index + 1).some(otherResult =>
+        result.result === otherResult.result
+      )
+    );
 
-      // If damage > 15, reduce HP of targeted token
-      if (damageRoll.total > 15) {
-        if (targets.length > 0) {
-          const armor = 15; // or use your actual armor property
-          const effectiveDamage = Math.max(damageRoll.total - armor, 0);
-          const currentHp = getProperty(targetActor, "system.hp") || 0;
-          const newHp = Math.max(currentHp - effectiveDamage, 0);
-          await targetActor.update({"system.hp": newHp});
-          ui.notifications.info(`Target ${targetActor.name} takes damage! HP reduced.`);
-        } 
-      }
-});
+    // Critical hit message
+    if (hasPair) {
+      ui.notifications.info(`${this.actor.name} deals critical hit to ${targetActor.name}!`);
+    } 
 
-   
-};
+    // Damage roll initialization
+    let dmgDice = boostedDmg ? 3 : 2;
+    let damageRoll = new Roll(`${dmgDice}d6 + ${pow}`);
+    await damageRoll.evaluate({ async: true });
+
+    // Damage roll message
+    damageRoll.toMessage({
+      speaker: ChatMessage.getSpeaker({ actor: this.actor }),
+      flavor: `${item.name} Damage Roll (${dmgDice}d6${boostedDmg ? ' Boosted' : ''})`,
+    });
+
+    // Wait for 3 seconds
+    await new Promise(resolve => setTimeout(resolve, 3000));
+
+    // Damage result check
+    if (damageRoll.total > 15) {
+      const armor = 15; 
+      const effectiveDamage = Math.max(damageRoll.total - armor, 0);
+      const currentHp = getProperty(targetActor, "system.hp") || 0;
+      const newHp = Math.max(currentHp - effectiveDamage, 0);
+      await targetActor.update({ "system.hp": newHp });
+      ui.notifications.info(`Target ${targetActor.name} takes damage! HP reduced.`);
+    }
+  }
 }
